da7b8839859c6715395a77cca53c68da2c2e9d8f
Nurkaiyr <yedige21@gmail.com>
2017-11-21 00:22:08 +0600

    Beep WriteLine ReadLine

---
+++ hw1/hw1/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace hw1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Beep();
+            Console.WriteLine("Git");
+            Console.ReadLine();
+        }
+    }
+}
